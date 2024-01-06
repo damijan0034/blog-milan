cfa52b7aecf6fa537165bca3bf922d2ff8d23cfe
Goran Kostic <damijan0034@gmail.com>
2024-01-06 11:29:57 +0100

    new

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,8 +7,8 @@ const Portfolio = () => {
     <div className=''>
         
         <h2 className='font-bold text-[20px] my-4'>Choose A Gallery</h2>
-        <div className=' sm:flex space-y-5 sm:space-x-10 '> 
-        <div className='flex-1 relative h-[400px] border mt-5 ' style={{backgroundImage:"url('/illustration.png')",backgroundSize:"cover"}} >
+        <div className=' md:flex space-y-5 sm:space-x-10   '> 
+        <div className='flex-1 relative h-[400px] w-full  border mt-5 ' style={{backgroundImage:"url('/illustration.png')",backgroundSize:"cover"}} >
             <Link href={"/portfolio/illustrations"}>
            
                 <h2 className='absolute bottom-2 text-2xl 
@@ -16,14 +16,14 @@ const Portfolio = () => {
             </Link>
             </div>
             
-            <div className='flex-1 relative h-[400px] border   ' style={{backgroundImage:"url('/apps.jpg')",backgroundSize:"cover"}} >
+            <div className='flex-1  relative h-[400px] w-full border   ' style={{backgroundImage:"url('/apps.jpg')",backgroundSize:"cover"}} >
             <Link href={"/portfolio/applications"}>
            
                 <h2 className='absolute bottom-2 text-2xl 
                 font-bold left-2'>APPLICATIONS</h2>   
             </Link>
             </div>
-            <div className='flex-1 relative h-[400px] border   ' style={{backgroundImage:"url('/websites.jpg')",backgroundSize:"cover"}} >
+            <div className='flex-1 relative h-[400px] w-full border   ' style={{backgroundImage:"url('/websites.jpg')",backgroundSize:"cover"}} >
             <Link href={"/portfolio/websites"}>
            
                 <h2 className='absolute bottom-2 text-2xl 
