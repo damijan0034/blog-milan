3071207401e8813d2904cac63a309ebd3c85d849
Goran Kostic <damijan0034@gmail.com>
2024-01-06 08:56:55 +0100

    add bgImage

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,37 +4,33 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>
+    <div className=''>
         
         <h2 className='font-bold text-[20px] my-4'>Choose A Gallery</h2>
-        <div className='sm:flex sm:space-x-10 '> 
-            
-            <Link href={"/portfolio/applications"}
-            className='flex-1 w-full h-[400px] relative border  my-10 '>
-                <Image src="/apps.jpg" alt='illustration' fill
-                className='object-cover '
-                
-                />
+        <div className=' sm:flex space-y-5 sm:space-x-10 '> 
+        <div className='flex-1 relative h-[400px]   ' style={{backgroundImage:"url('/illustration.png')",backgroundSize:"cover"}} >
+            <Link href={"/portfolio/illustrations"}>
+           
                 <h2 className='absolute bottom-2 text-2xl 
-                font-bold left-2'>APPLICATIONS</h2>
+                font-bold left-2'>ILLUSTRATIONS</h2>   
             </Link>
+            </div>
             
-            <Link href={"/portfolio/illustrations"}
-            className='flex-1 w-full border border-3 h-[400px] relative my-10 '>
-                <Image src="/illustration.png" alt='contact' fill
-                className='object-cover '
-                />
-                 <h2 className='absolute bottom-2 text-2xl 
-                font-bold left-2'>ILLUSTRATIONS</h2>
+            <div className='flex-1 relative h-[400px]   ' style={{backgroundImage:"url('/apps.jpg')",backgroundSize:"cover"}} >
+            <Link href={"/portfolio/applications"}>
+           
+                <h2 className='absolute bottom-2 text-2xl 
+                font-bold left-2'>APPLICATIONS</h2>   
             </Link>
-            <Link href={"/portfolio/websites"}
-             className='flex-1 w-full border border-3 h-[400px] relative my-10  '>
-                <Image src="/websites.jpg" alt='contact' fill
-                className='object-cover '
-                />
-                 <h2 className='absolute bottom-2 text-2xl 
-                font-bold left-2'>WEBSITES</h2>
+            </div>
+            <div className='flex-1 relative h-[400px]   ' style={{backgroundImage:"url('/websites.jpg')",backgroundSize:"cover"}} >
+            <Link href={"/portfolio/websites"}>
+           
+                <h2 className='absolute bottom-2 text-2xl 
+                font-bold left-2'>WEBSITES</h2>   
             </Link>
+            </div>
+            
             
         </div>
     </div>
